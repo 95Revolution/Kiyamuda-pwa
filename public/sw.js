9e742dcbe0f11ef42ede51de029f2f27849f14ee
Devin Herath <devdhera@gmail.com>
2018-08-18 12:56:05 +0530

    Configured Dynamic Cache Cleaning

--- public/sw.js
+++ public/sw.js
@@ -17,6 +17,16 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
+function trimCache(cacheName, maxItems) {
+  caches.open(cacheName).then(function(cache) {
+    return cache.keys().then(function(keys) {
+      if (keys.length > maxItems) {
+        cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+      }
+    });
+  });
+}
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ... ', event);
   event.waitUntil(
@@ -61,6 +71,7 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
         return fetch(event.request).then(function(res) {
+          trimCache(CACHE_DYNAMIC_NAME, 3);
           cache.put(event.request, res.clone());
           return res;
         });
@@ -77,6 +88,7 @@ self.addEventListener('fetch', function(event) {
           return fetch(event.request)
             .then(function(res) {
               return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+                trimCache(CACHE_DYNAMIC_NAME, 3);
                 cache.put(event.request.url, res.clone());
                 return res;
               });
