1d1f2d85fd19b4d12a7f87dfc2bc7ae37c7065b3
Devin Herath <devdhera@gmail.com>
2018-08-19 20:05:16 +0530

    Configured subscription management func

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -56,18 +56,34 @@ function displayConfirmNotification() {
   }
 }
 
+function configurePushSub() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  navigator.serviceWorker.ready
+    .then(function(swreg) {
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function(sub) {
+      if (sub === null) {
+      }
+    });
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission(function(result) {
     console.log('User Choice', result);
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-      displayConfirmNotification();
+      configurePushSub();
+      // displayConfirmNotification();
     }
   });
 }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
     enableNotificationsButtons[i].style.display = 'inline-block';
     enableNotificationsButtons[i].addEventListener(
