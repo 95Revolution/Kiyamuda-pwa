20559d7e1f9fec07584ce956bd5dd842f3d0bde6
Devin Herath <devdhera@gmail.com>
2018-08-19 19:02:07 +0530

    Added confirm notification

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -25,12 +25,20 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
+function displayConfirmNotification() {
+  var options = {
+    body: 'You successfully subscribed to our Kiyamuda Notification Service!'
+  };
+  new Notification('Successfully Subscribsed!', options);
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission(function(result) {
     console.log('User Choice', result);
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
+      displayConfirmNotification();
     }
   });
 }
